8c02927222c1fd048e3733ffedba5b2c04079e0d
zhengshaorui <845731923@qq.com>
2019-02-12 11:35:57 +0800

    添加 jar

--- app/src/main/java/com/ist/lifecycle/MainActivity.java
+++ app/src/main/java/com/ist/lifecycle/MainActivity.java
@@ -1,9 +1,7 @@
 package com.ist.lifecycle;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ist.lifecyclerlib.ZLifeCycle;
 import com.ist.lifecyclerlib.listener.LifeListenerAdapter;
@@ -14,45 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        startService(new Intent(this,TestService.class));
-
-        ZLifeCycle.with(this,new LifeListenerAdapter() {
-            @Override
-            public void onStart() {
-                super.onStart();
-                Log.d(TAG, "zsr --> onStart: ");
-            }
-
-            @Override
-            public void onResume() {
-                super.onResume();
-                Log.d(TAG, "zsr --> onResume: ");
-            }
-
+        ZLifeCycle.with(this, new LifeListenerAdapter() {
             @Override
             public void onPause() {
                 super.onPause();
-                Log.d(TAG, "zsr --> onPause: ");
-            }
-
-            @Override
-            public void onStop() {
-                super.onStop();
-                Log.d(TAG, "zsr --> onStop: ");
             }
 
             @Override
             public void onDestroy() {
                 super.onDestroy();
-                Log.d(TAG, "zsr --> onDestroy: ");
             }
 
             @Override
             public void onFail(String errorMsg) {
                 super.onFail(errorMsg);
-                Log.d(TAG, "zsr --> onFail: "+errorMsg);
             }
         });
+
+
     }
 }
